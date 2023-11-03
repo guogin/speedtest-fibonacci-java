fda07206b7dcd42cc8fad5aee14eab81ed1bb090
Guo, Yong Kang <yong.kang.guo@sap.com>
2023-11-03 22:59:06 +0800

    minor change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci();
-        System.out.println(fib.recursive_no_optimization(40));
+        System.out.println(fib.recursive_no_optimization(30));
     }
 }
