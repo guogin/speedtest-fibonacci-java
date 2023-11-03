c84c3c15108774acfadfd405427af0ec14a1fdf7
Guo, Yong Kang <yong.kang.guo@sap.com>
2023-11-03 17:41:25 +0800

    test program

---
+++ src/main/java/org/example/Fibonacci.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+    public BigInteger recursive(int n) {
+        if (n < 2) {
+            return BigInteger.valueOf(n);
+        }
+
+        BigInteger memory[] = new BigInteger[n+1];
+        return _internal_recursive(n, memory);
+    }
+
+    private BigInteger _internal_recursive(int n, BigInteger[] memory) {
+        if (n < 2) {
+            memory[n] = BigInteger.valueOf(n);
+            return memory[n];
+        }
+        if (memory[n] != null) {
+            return memory[n];
+        }
+
+        memory[n] = _internal_recursive(n-1, memory).add(
+                _internal_recursive(n-2, memory));
+
+        return memory[n];
+    }
+
+    public BigInteger iterative(int n) {
+        if (n < 2) {
+            return BigInteger.valueOf(n);
+        }
+
+        BigInteger n0 = BigInteger.valueOf(0);
+        BigInteger n1 = BigInteger.valueOf(1);
+        BigInteger ni = BigInteger.valueOf(0);
+
+        for (int i = 2; i <= n; ++i) {
+            ni = n0.add(n1);
+            // step forward
+            n0 = n1;
+            n1 = ni;
+        }
+
+        return ni;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Fibonacci fib = new Fibonacci();
+        System.out.println(fib.recursive(100));
+    }
+}
